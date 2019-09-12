05d36ee9220b803dcaf66cfc573a8516e26eacb6
rusrom <rusrom@guyfawkes.33mail.com>
2019-09-12 10:39:19 +0300

    Asyncio and aiohttp fundamentals

---
+++ 17_1_Asyncron/5_asincyo_aiohttp_parser.py
@@ -0,0 +1,78 @@
+import aiohttp
+import asyncio
+from pprint import pprint
+from parsel import Selector
+
+# py4seo variant
+# async def main():
+#     session = aiohttp.ClientSession()
+#     response = await session.get('http://python.org')
+#     html = await response.text()
+#     sel = Selector(text=html)
+#     print(dir(sel))
+#     await session.close()
+
+domain = 'kartochki-domana.com.ua'
+queue = asyncio.Queue()
+added_prod_urls = set()
+
+# My variant
+async def worker(session):
+    while not queue.empty():
+        target_url = await queue.get()
+        async with session.get(target_url) as response:
+            html = await response.text()
+        
+        sel = Selector(text=html)
+
+        title = sel.xpath('//h1[contains(@class, "product_title")]/text()').get()
+        price = sel.xpath('//p[@class="price"]/span[contains(@class, "woocommerce-Price-amount")]/text()').get()
+
+        print(f'{title}: {price}')
+        
+        # [
+        #     (await queue.put(prod_url), added_prod_urls.add(prod_url))
+        #     for prod_url in prod_urls
+        #     if prod_url not in added_prod_urls
+        # ]
+
+
+async def main(): 
+    async with aiohttp.ClientSession() as session:
+        response = await session.get('https://kartochki-domana.com.ua/ru/product-category/podarochnie-nabori/')
+        html = await response.text()
+        
+        sel = Selector(text=html)
+        prod_urls = sel.xpath('//h3[@class="product_title"]/a/@href').getall()
+
+        [
+            (await queue.put(prod_url), added_prod_urls.add(prod_url))
+            for prod_url in prod_urls
+            if prod_url not in added_prod_urls
+        ]
+        
+        print(queue.qsize())
+        # pprint(added_prod_urls)
+        
+        tasks = []
+        for _ in range(50):
+            task = asyncio.Task(worker(session))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+
+
+    # task1 = asyncio.Task(coro())
+    # task2 = asyncio.Task(main())
+
+    # runer = asyncio.gather(task1, task2)
+
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(runer)
+
+
+    # Run just 1 coroutine main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
